33fc08c1894696d79420b588c7f8068984ea29dd
Balasaheb Nawale <balasaheb.nawale@worldline.com>
2024-06-21 00:10:40 +0530

    added code for PWA

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer class=" fixed bottom-0 flex  w-full h-16  bg-gray-300 shadow dark:bg-gray-800  justify-center items-center text-center">
-      <div class="text-sm h-6 text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className=" fixed bottom-0 flex  w-full h-16  bg-gray-300 shadow dark:bg-gray-800  justify-center items-center text-center">
+      <div className="text-sm h-6 text-gray-500 sm:text-center dark:text-gray-400">
         © 2023 . All Rights Reserved.
       </div>
     </footer>
