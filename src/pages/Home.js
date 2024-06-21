c5ed4ca2fbbb0770b785af5a990a14b215af88c5
Balasaheb Nawale <balasaheb.nawale@worldline.com>
2024-06-21 12:48:37 +0530

    added cursor on bookings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -87,11 +87,10 @@ function Home() {
         </div>
         <div className="lg: w-11/12 md:w-[750px] flex justify-around my-20 mx-auto gap-2">
           <Card
-            className="lg:w-1/3 md:w-2/5 flex flex-col items-center justify-center text-center"
+            className="lg:w-1/3 md:w-2/5 flex flex-col items-center justify-center text-center hover:cursor-pointer"
             title="My Bookings"
             icon={<FcBusinessman size={200} />} // Replace with your image URL
             onClick={() => {
-              
               return navigate("/userbooking");
             }}
           />
